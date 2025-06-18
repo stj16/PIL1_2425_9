52f4a5f04818775851ddfba5d2f1b4ddeaf41b2f
stj16 <setondjiosias3@gmail.com>
2025-06-18 23:42:46 +0100

    back

---
+++ ifri_pi9_backend/offer/migrations/0005_add_day_field.py
@@ -0,0 +1,14 @@
+# Generated manually to sync with existing database
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('offer', '0003_remove_caroffer_address_caroffer_start_point_usual'),
+    ]
+
+    operations = [
+        # Migration vide - le champ day existe déjà dans la base de données
+    ]
